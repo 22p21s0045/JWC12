bdb23f83f1f3789273ced21ede8c138e71c6e933
lommel <monkeytitle@hotmail.com>
2022-06-17 11:42:01 +0700

    pre: add animation

--- components/review/Review.tsx
+++ components/review/Review.tsx
@@ -1,8 +1,19 @@
 import React from "react";
-import { Grid, Typography, Box, Stack, Paper, Avatar } from "@mui/material";
-
+import { useEffect } from "react";
+import { Grid, Typography, Box, Stack, Paper, Avatar,Grow } from "@mui/material";
+import rating from "../../public/lottie/rating.json";
 import Image from "next/image";
+import Lottie from "lottie-react";
+import AOS from "aos";
+import { useInView } from 'react-intersection-observer';
+
 function Review() {
+  const [ref, inView] = useInView();
+  useEffect(() => {
+   console.log(inView)
+  }, [inView]);
+  
+
   return (
     <div>
       <Grid
@@ -10,62 +21,82 @@ function Review() {
         spacing={3}
         sx={{ marginTop: "10%" }}
         justifyContent="center"
+        
       >
         <Grid item xs={12} lg={12} md={12}>
           <Box sx={{ textAlign: "center" }}>
             <Typography sx={{ fontFamily: "Mitr", fontSize: "3rem" }}>
               รีวิวจากลูกค้า
             </Typography>
+            <Lottie
+              animationData={rating}
+              loop={true}
+              style={{
+                width: 400,
+                height: 400,
+                position: "relative",
+                left: "40%",
+              }}
+            />
           </Box>
         </Grid>
         <Grid
           container
           spacing={3}
           justifyContent="center"
-          sx={{ marginTop: 7 }}
+          sx={{ marginTop: 1 }}
+          ref={ref}
         >
+          
+
           <Grid item xs={12} lg={3} md={4}>
-            <Paper sx={{ backgroundColor: "#F6E9E1" }}>
-              <Box
-                sx={{
-                  textAlign: "center",
-                  position: "relative",
-                  left: "40%",
-                  paddingTop: 5,
-                }}
-              >
-                <Avatar
-                  alt="Remy Sharp"
-                  src="/img/review/man.png"
-                  sx={{ width: 100, height: 100, backgroundColor: "#F9F2ED" }}
-                />
-              </Box>
-              <Typography
-                sx={{
-                  fontSize: "1.5rem",
-                  fontFamily: "Mitr",
-                  textAlign: "center",
-                }}
-              >
-                เเมททิว นักธุรกิจ
-              </Typography>
-              <Typography
-                sx={{
-                  fontSize: "1rem",
-                  fontFamily: "Mitr",
-                  textAlign: "center",
-                  marginTop: 4,
-                  paddingBottom: 5,
-                  paddingLeft: 5,
-                  paddingRight: 5,
-                }}
-              >
-                หมอหมึกช่วยผมในฤกษ์ยามในการเจรจาธุรกิจได้มากเลยตอนนี้ขายดีเทนํ้าเทท่าเลยครับ
-              </Typography>
-            </Paper>
+           
+              <Paper sx={{ backgroundColor: "#F6E9E1" }}>
+
+                <Box
+                  sx={{
+                    textAlign: "center",
+                    position: "relative",
+                    left: "40%",
+                    paddingTop: 5,
+                  }}
+                >
+                  <Avatar
+                    alt="Remy Sharp"
+                    src="/img/review/man.png"
+                    sx={{ width: 100, height: 100, backgroundColor: "#F9F2ED" }}
+                  />
+                </Box>
+                <Typography
+                  sx={{
+                    fontSize: "1.5rem",
+                    fontFamily: "Mitr",
+                    textAlign: "center",
+                  }}
+                >
+                  เเมททิว นักธุรกิจ
+                </Typography>
+                <Typography
+                  sx={{
+                    fontSize: "1rem",
+                    fontFamily: "Mitr",
+                    textAlign: "center",
+                    marginTop: 4,
+                    paddingBottom: 5,
+                    paddingLeft: 5,
+                    paddingRight: 5,
+                  }}
+                >
+                  หมอหมึกช่วยผมในฤกษ์ยามในการเจรจาธุรกิจได้มากเลยตอนนี้ขายดีเทนํ้าเทท่าเลยครับ
+                </Typography>
+
+              </Paper>
+            
+
           </Grid>
+
           <Grid item xs={12} lg={3} md={4}>
-            <Paper sx={{ backgroundColor: "#EADEDE",marginTop:10 }}>
+            <Paper sx={{ backgroundColor: "#EADEDE", marginTop: 10 }}>
               <Box
                 sx={{
                   textAlign: "center",
@@ -145,6 +176,7 @@ function Review() {
               </Typography>
             </Paper>
           </Grid>
+         
         </Grid>
       </Grid>
     </div>
