df94d58fa71a11599496d0c5f4288f83bc4420ca
lommel <monkeytitle@hotmail.com>
2022-06-17 20:21:44 +0700

    add: lent page success

--- pages/lent.tsx
+++ pages/lent.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Grid, TextField,Stack } from "@mui/material";
+import { Grid, TextField, Stack } from "@mui/material";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import Button from '@mui/material/Button';
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import Button from "@mui/material/Button";
 import { useState } from "react";
+import swal from "sweetalert2";
 function lent() {
   const [date, setDate] = useState(null);
   const [name, setName] = useState(null);
@@ -12,27 +13,56 @@ function lent() {
   const handle_date = (newvalue: any) => {
     setDate(newvalue);
   };
+  const handle_name = (event: any) => {
+    setName(event.target.value);
+  };
+  const handle_phone = (event: any) => {
+    setPhone(event.target.value);
+  };
+  const handle_submit = () => {
+    if (date && name && phone) {
+      swal
+        .fire({
+          title: "ยืนยันการจอง",
+
+          text: `วันที่จอง ${new Date(date).toLocaleDateString()} ชื่อ ${name} เบอร์โทร ${phone}`,
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "ยืนยัน",
+          cancelButtonText: "ยกเลิก",
+        })
+        .then((result) => {
+          if (result.value) {
+            swal.fire(
+              "จองหมอหมึกสำเร็จ",
+              "ขอบคุณสําหรับการจอง",
+              "success"
+            );
+          }
+        });
+    } else {
+      swal.fire({
+        title: "กรุณากรอกข้อมูลให้ครบถ้วน",
+        text: "กรุณากรอกข้อมูลให้ครบถ้วน",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "ยืนยัน",
+        cancelButtonText: "ยกเลิก",
+      });
+    }
+  };
+
   return (
     <div>
-      <Grid
-        container
-        spacing={3}
-        sx={{
-          position: "absolute",
-          width: "100%",
-          height: "100%",
-          backgroundColor: "pink",
-        }}
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Grid item xs={12} lg={4}>
-          <TextField label="ชื่อ" />
-        </Grid>
-        <Grid item xs={12} lg={4}>
-          <TextField label="เบอร์โทร" />
-        </Grid>
-        <Grid item xs={12} lg={4}>
+      <Stack spacing={3}>
+        <TextField label="ชื่อ" onChange={handle_name} />
+
+        <TextField label="เบอร์โทร" onChange={handle_phone} />
+
         <LocalizationProvider dateAdapter={AdapterDateFns}>
           <DateTimePicker
             label="เลือกเวลา"
@@ -41,11 +71,9 @@ function lent() {
             renderInput={(params) => <TextField {...params} />}
           />
         </LocalizationProvider>
-        <Grid item xs={12} lg={12}>
-            <Button>จองเลย</Button>
-            </Grid>
-        </Grid>
-      </Grid>
+
+        <Button onClick={handle_submit}>จองเลย</Button>
+      </Stack>
     </div>
   );
 }
